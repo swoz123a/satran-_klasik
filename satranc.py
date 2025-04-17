4ae744550c08023baff98aaec79acb5984d1da36
swoz123a <etiket5000@gmail.com>
2025-04-17 22:19:00 +0300

    Create satranc.py

---
+++ satranc.py
@@ -0,0 +1,19 @@
+import pygame as p
+
+
+# ChessEngine.py içindeki sınıfın basit bir örneği
+class GameState:
+    def __init__(self):
+        # Örnek olarak 8x8 satranç tahtası
+        # "--" boş kareleri temsil eder
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove = True
+        self.moveLog = []
